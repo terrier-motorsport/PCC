7a2a481eb816d69b617affad16d9426a33b6bdbe
jesseAMRamos <jesse637@bu.edu>
2025-03-08 17:22:18 -0500

    standardize project structure

--- main.js
+++ main.js
@@ -1,2 +1,32 @@
-let phrase = 'bananna!';
-console.log(phrase);
+const vtabs_chart = document.getElementById("voltage_tabs");
+const therms_chart = document.getElementById("thermistors");
+
+
+function AMS_Page(){
+    new Chart(vtabs_chart, {
+      type: "pie",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+          },
+        ],
+      },
+    });
+    new Chart(therms_chart, {
+      type: "pie",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+          },
+        ],
+      },
+    });
+}
